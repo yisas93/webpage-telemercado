519be4307cadf81de14395cc7dcb6b98975c2188
yisas93 <juan249325@gmail.com>
2023-05-12 18:29:08 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './views/App.jsx';
 import reportWebVitals from './reportWebVitals';
+import { Videos } from './views/videos.jsx';
 
+import {
+  createBrowserRouter, RouterProvider
+ } from "react-router-dom"
+
+ const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/videos",
+    element: <Videos/>
+  }
+ ])
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/views/videos.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+export const Videos=()=>{
+    return(
+        <h1>videos</h1>
+    )
+
+}
